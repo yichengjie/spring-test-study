f6a18818a7042abe66f162fde676a16b95c1fb2f
易成杰 <626659321@qq.com>
2023-10-09 21:15:35 +0800

    save

---
+++ hello-webmvc/src/main/java/com/yicj/study/webmvc/WebMvcApplication.java
@@ -0,0 +1,16 @@
+package com.yicj.study.webmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yicj
+ * @date 2023/10/9 21:12
+ */
+@SpringBootApplication
+public class WebMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebMvcApplication.class, args) ;
+    }
+}
---
+++ hello-webmvc/src/main/java/com/yicj/study/webmvc/config/AppConfig.java
@@ -0,0 +1,12 @@
+package com.yicj.study.webmvc.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author yicj
+ * @date 2023/10/9 21:15
+ */
+@Configuration
+public class AppConfig {
+
+}
