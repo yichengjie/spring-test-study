2815ac62a55f91293f8885f878548d89471113bb
yichengjie <626659321@qq.com>
2023-11-03 14:05:25 +0800

    mvc层单元测试保存

--- hello-webmvc/src/test/java/com/yicj/study/webmvc/controller/HelloController3Test.java
+++ hello-webmvc/src/test/java/com/yicj/study/webmvc/controller/HelloController3Test.java
@@ -1,25 +1,18 @@
 package com.yicj.study.webmvc.controller;
 
-import com.baomidou.mybatisplus.autoconfigure.MybatisPlusAutoConfiguration;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.yicj.study.webmvc.WebMvcApplication;
 import com.yicj.study.webmvc.model.form.HelloCreateForm;
-import com.yicj.study.webmvc.repository.mapper.StudentMapper;
 import com.yicj.study.webmvc.service.HelloService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.web.context.WebApplicationContext;
-
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
